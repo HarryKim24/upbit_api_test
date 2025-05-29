a9dfb991e97d50be95bd04e5684036300512aff5
HarryKim24 <tl9434@naver.com>
2025-05-29 13:25:09 +0900

    api 작성

---
+++ src/types/upbitTypes.ts
@@ -0,0 +1,22 @@
+export interface MarketItem {
+  market: string;
+  korean_name: string;
+  english_name: string;
+  market_event?: {
+    warning: boolean;
+    caution: {
+      PRICE_FLUCTUATIONS: boolean;
+      TRADING_VOLUME_SOARING: boolean;
+      DEPOSIT_AMOUNT_SOARING: boolean;
+      GLOBAL_PRICE_DIFFERENCES: boolean;
+      CONCENTRATION_OF_SMALL_ACCOUNTS: boolean;
+    }
+  }
+}
+
+export interface TickerItem {
+  market: string;
+  trade_price: number;
+  change: "RISE" | "FAIL" | "EVEN";
+  change_rate: number;
+}
